5edac2b14c8994034ee4d6647258b5ca0fa80395
Randy Tarampi <rt@randytarampi.ca>
2017-07-06 14:15:32 -0700

    Parameterize the patterns used to match notifications to their handlers.
    I have preexisting SNS topics that I just want to listen in on, and I didn't want to have to create new ones just to use this out of the box.
    
    I got fed up with having outstanding changes that pulling it out was probably a good idea, especially considering assertible/lambda-cloudwatch-slack#16.

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
   kmsEncryptedHookUrl: process.env.KMS_ENCRYPTED_HOOK_URL, // encrypted slack webhook url
   unencryptedHookUrl: process.env.UNENCRYPTED_HOOK_URL,    // unencrypted slack webhook url
   slackChannel: process.env.SLACK_CHANNEL,                 // slack channel to send a message to
-  slackUsername: process.env.SLACK_USERNAME,               // "AWS SNS via Lamda", // slack username to user for messages
+  slackUsername: process.env.SLACK_USERNAME,               // "AWS SNS via Lambda", // slack username to user for messages
   icon_emoji: process.env.ICON_EMOJI,                      // slack emoji icon to use for messages
   orgIcon: process.env.ORG_ICON,                           // url to icon for your organization for display in the footer of messages
   orgName: process.env.ORG_NAME,                           // name of your organization for display in the footer of messages
@@ -11,24 +11,24 @@ module.exports = {
   services: {
     elasticbeanstalk: {
       // text in the sns message or topicname to match on to process this service type
-      match_text: "ElasticBeanstalkNotifications"
+      match_text: process.env.ELASTIC_BEANSTALK_NOTIFICATION_MATCH_TEXT || "ElasticBeanstalkNotifications"
     },
     cloudwatch: {
       // text in the sns message or topicname to match on to process this service type
-      match_text: "CloudWatchNotifications"
+      match_text: process.env.CLOUDWATCH_NOTIFICATION_MATCH_TEXT || "CloudWatchNotifications"
     },
     codedeploy: {
       // text in the sns message or topicname to match on to process this service type
-      match_text: "CodeDeploy"
+      match_text: process.env.CODEDEPLOY_NOTIFICATION_MATCH_TEXT || "CodeDeploy"
     },
     elasticache: {
       // text in the sns message or topicname to match on to process this service type
-      match_text: "ElastiCache"
+      match_text: process.env.ELASTICACHE_NOTIFICATION_MATCH_TEXT || "ElastiCache"
     },
     autoscaling: {
       // text in the sns message or topicname to match on to process this service type
-      match_text: "AutoScaling"
+      match_text: process.env.AUTO_SCALING_NOTIFICATION_MATCH_TEXT || "AutoScaling"
     }
   }
 
-}
+};
